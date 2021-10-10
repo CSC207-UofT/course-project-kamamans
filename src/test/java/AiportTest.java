3c99a5027a1ce43b70670efd1fab09305ad7c979
sms-sudo <irock12022@gmail.com>
2021-10-10 15:36:14 -0400

    completed AirportTest with the exception of methods requiring Routes

--- src/test/java/AiportTest.java
+++ src/test/java/AiportTest.java
@@ -1,2 +1,48 @@
+
+import entities.Airport;
+import entities.Route;
+import entities.basicUser;
+import org.junit.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
 public class AiportTest {
+    Airport a;
+    private Route r;
+    //TODO : need to complete Route before testing
+    List<Route> listofRoute = new ArrayList<Route>(List.of(r));
+
+    @Before
+    public void setUp() throws Exception {
+        a = new Airport("Toronto", "YYZ", listofRoute);
+    }
+
+    @Test(timeout = 50)
+    public void TestGetCity(){
+        assertEquals("Toronto", a.getCity());
+    }
+
+    @Test(timeout = 50)
+    public void TestSetCity(){
+        a.setCity("GTA");
+        assertEquals("GTA", a.getCity());
+    }
+
+    @Test(timeout = 50)
+    public void TestGetIataCode(){
+        assertEquals("YYZ", a.getIataCode());
+    }
+
+    @Test(timeout = 50)
+    public void TestSetIataCode(){
+        a.setIataCode("YTZ");
+        assertEquals("YTZ", a.getIataCode());
+    }
+
+    //TODO: Once Routes is created complete Unit Test Cases
+
 }
