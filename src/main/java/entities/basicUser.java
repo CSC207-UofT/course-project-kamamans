57948add36c2d65899abc1b0f4936253aed57184
sms-sudo <irock12022@gmail.com>
2021-10-10 14:30:41 -0400

    filling in basicUser class, re-CRC cards specifications created loginInformation that is extended to include hashmap to access and set username/passwords

--- src/main/java/entities/basicUser.java
+++ src/main/java/entities/basicUser.java
@@ -1,4 +1,48 @@
 package entities;
 
-public class basicUser {
+public class basicUser extends loginInformation{
+    private String ID;
+    private String email;
+    private int phoneNumber;
+    private String classType;
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getClassType() {
+        return classType;
+    }
+
+    public void setClassType(String classType) {
+        this.classType = classType;
+    }
+
+    public basicUser(String id, String email, int phoneNumber, String c){
+        this.ID = id;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.classType = c;
+    }
+
 }
