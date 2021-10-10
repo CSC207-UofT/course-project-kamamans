129ceb972cf9e4bf30184e187c8a89e9baefa1aa
sms-sudo <irock12022@gmail.com>
2021-10-10 13:24:01 -0400

    filling in Airport class, re-CRC cards specifications

--- src/main/java/entities/Airport.java
+++ src/main/java/entities/Airport.java
@@ -1,4 +1,37 @@
 package entities;
 
+import java.util.List;
+
 public class Airport {
+    private String city;
+    private String iataCode;
+    private List<Route> routes;
+
+    public void setCity(String city){
+        this.city = city;
+    }
+
+    public String getCity(){
+        return this.city;
+    }
+
+    public void setIataCode(String IATA){
+        this.iataCode = IATA;
+    }
+
+    public String getIataCode(){
+        return this.iataCode;
+    }
+
+    public void setRoutes(List<Route> routes){
+        this.routes = routes;
+    }
+
+    public void addRoute(Route route){
+        this.routes.add(route);
+    }
+
+    public List<Route> getRoutes(){
+        return this.routes;
+    }
 }
