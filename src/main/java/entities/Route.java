e018592d30210413ff2e875cf644e6f4ee13a4df
zhaoand9 <ae.zhao@mail.utoronto.ca>
2021-10-13 13:43:45 -0400

    Route and TestRoute

--- src/main/java/entities/Route.java
+++ src/main/java/entities/Route.java
@@ -1,4 +1,27 @@
 package entities;
 
-public class Route {
+public class Route<String> {
+    private String DepartureAirport;
+    private String DestinationAirport;
+
+    public Route(String DepartureAirport, String DestinationAirport) {
+        this.DepartureAirport = DepartureAirport;
+        this.DestinationAirport = DestinationAirport;
+    }
+
+    public String getDepartureAirport() {
+        return this.DepartureAirport;
+    }
+
+    public String getDestinationAirport() {
+        return this.DestinationAirport;
+    }
+
+    public void setDepartureAirport(String newDepartureAirport){
+        this.DepartureAirport = newDepartureAirport;
+    }
+
+    public void setDestinationAirport(String newDestinationAirport){
+        this.DestinationAirport = newDestinationAirport;
+    }
 }
--- src/test/java/RouteTest.java
+++ src/test/java/RouteTest.java
@@ -1,2 +1,30 @@
+import entities.Route;
+
+
 public class RouteTest {
+    public Route r;
+
+    @Before
+    public void setUp() throws Exception {
+        r = new Route("Toronto", "Edmonton");
+    }
+
+    @Test(timeout = 50)
+    public void TestgetDepartureAirport(){
+        assertEquals("Toronto", r.getDepartureAirport());
+    }
+
+    @Test(timeout = 50)
+    public void TestgetDestinationAirport(){
+        assertEquals("Edmonton", r.getDestinationAirport());
+    }
+    @Test(timeout = 50)
+    public void TestsetDepartureAirport("Tampa Bay"){
+        assertEquals("Tampa Bay", r.getDepartureAirport());
+    }
+
+    @Test(timeout = 50)
+    public void TestsetDestinationAirport("Tampa Bay"){
+        assertEquals("Tampa Bay", r.setDestinationAirport());
+    }
 }
