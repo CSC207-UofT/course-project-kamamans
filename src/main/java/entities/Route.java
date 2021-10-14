4c8b9c78dd041b252d68c2728df6bc5a697c5ee8
zhaoand9 <ae.zhao@mail.utoronto.ca>
2021-10-13 22:33:15 -0400

    Route and TestRoute added duration and price

--- src/main/java/entities/Route.java
+++ src/main/java/entities/Route.java
@@ -3,10 +3,14 @@ package entities;
 public class Route<String> {
     private String departureAirport;
     private String destinationAirport;
+    private double priceofFlights;
+    private double totalDuration;
 
-    public Route(String departureAirport, String destinationAirport) {
+    public Route(String departureAirport, String destinationAirport, double priceofFlights, double totalDuration) {
         this.departureAirport = departureAirport;
         this.destinationAirport = destinationAirport;
+        this.priceofFlights = priceofFlights;
+        this.totalDuration = totalDuration;
     }
 
     public String getDepartureAirport() {
@@ -21,7 +25,15 @@ public class Route<String> {
         this.departureAirport = newDepartureAirport;
     }
 
-    public void setDestinationAirport(String newDestinationAirport){
+    public void setDestinationAirport(String newDestinationAirport) {
         this.destinationAirport = newDestinationAirport;
+    }
+
+    public double getPriceofFlights(){
+        return this.priceofFlights;
+    }
+
+    public double getTotalDuration(){
+        return this.totalDuration;
     }
 }
