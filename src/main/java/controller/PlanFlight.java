28fef983265413740850f1192d5dc74f1a603a5b
NathanZC <nathanccox52@gmail.com>
2021-10-14 20:26:19 -0400

    Revert "Done PlanFlight"
    This reverts commit df9468cbf4f289094ca37214dd0117c5896dfb3f.

--- src/main/java/controller/PlanFlight.java
+++ src/main/java/controller/PlanFlight.java
@@ -1,28 +1,4 @@
 package controller;
 
-import entities.Airport;
-import entities.Flight;
-import usecases.InteractDatabase;
-
-import java.util.ArrayList;
-import java.util.Date;
-import entities.SearchQueries;
-import entities.Route;
 public class PlanFlight {
-
-    private Flight selectedFlight;
-
-
-    public SearchQueries<Route> EnterSearchRequirements(Date date, Airport departure, Airport destination ){
-        InteractDatabase db = new InteractDatabase();
-        ArrayList<Route> routes = db.routeByParameters( departure, destination, date);
-        return new SearchQueries<>(routes);
-    }
-    public void selectFlight(Flight flightToBeSelected){
-        this.selectedFlight = flightToBeSelected;
-    }
-    public Flight getSelectedFlight(){
-        return this.selectedFlight;
-    }
-
 }
