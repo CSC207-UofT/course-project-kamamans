3bb56e4f833d428c63d4ce3fbb5a084dfe7203ba
majdaloj <majda.lojpur@mail.utoronto.ca>
2021-10-16 18:57:44 -0400

    (Majda) CHANGES MADE TO: Flight, Plane, FlightTest, and PlaneTest.

--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,3 +1,4 @@
+import entities.Airport;
 import entities.Flight;
 import entities.Plane;
 import entities.Route;
@@ -12,34 +13,36 @@ import static org.junit.Assert.*;
 
 public class FlightTest {
     Flight f;
-    Route r;
     Plane p;
+    Route r1;
+    Route r2;
+    Airport a1;
+    Airport a2;
 
 
     @Before
     public void setup() throws Exception {
-        // create an arraylist
-        ArrayList<String> passengers = new ArrayList<String>();
-        passengers.add("Majda");
-        passengers.add("Salwa");
-        passengers.add("Nathan");
-        passengers.add("Kevin");
-        passengers.add("Albert");
-        passengers.add("Marian");
-        passengers.add("Andrew");
-        passengers.add("Andrei");
 
         // create an instance of Plane
 
         p = new Plane("Airbus A321", 220, 20,
-                200, true, passengers);
+                200, true);
+
+        // create two instances of Airport (one source and one destination point of the flight)
+
+        ArrayList<Route> routes = new ArrayList<>();
+        routes.add(r1);
+        routes.add(r2);
+
+        a1 = new Airport("Toronto", "YYZ", routes);
+        a2 = new Airport("London", "YXU", routes);
 
         // create an instance of Flight
         //Generate a date for Jan. 9, 2021, 10:11:12 AM
         Calendar cal = Calendar.getInstance();
         cal.set(2021, Calendar.JANUARY, 9, 10, 11, 12); //Year, month, day of month, hours, minutes and seconds
         Date date = cal.getTime();
-        f = new Flight(date, p, r, 1 );
+        f = new Flight(date, p, 1, a1, a2);
     }
 
     @Test(timeout = 50)
@@ -63,6 +66,15 @@ public class FlightTest {
         assert(1 == f.getDuration());
     }
 
-    //TODO: Create a unit test for Route, when Route Class is implemented
+    @Test(timeout = 50)
+    public void TestGetSource(){
+        assert(a1 == f.getSource());
+    }
+
+    @Test(timeout = 50)
+    public void TestGetDestination(){
+        assert(a2 == f.getDestination());
+    }
+
 
 }
