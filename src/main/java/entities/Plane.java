eb7de563a7efdff084086dba088d8c937d0dee94
zhaoand9 <ae.zhao@mail.utoronto.ca>
2021-11-28 21:47:52 -0500

    Simple docstring comments on classes and more complex methods for entities. Added todo issue with pricing on flights class.

--- src/main/java/entities/Plane.java
+++ src/main/java/entities/Plane.java
@@ -3,6 +3,11 @@ package entities;
 import java.io.Serializable;
 import java.util.ArrayList;
 
+/**
+ * Creates a plane object that stores the brand name of the plane, total seat count, number of first class seats,
+ * number of economy seats, and the number of vacant seats on the plane.
+ */
+
 public class Plane implements Serializable {
     private String brandName;
     private int seatCount;
