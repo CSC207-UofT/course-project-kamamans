35e4b332a2ce6cfb26c461f33cacb2beb4e351da
Albert Cao <alberthcao@gmail.com>
2021-12-04 10:10:20 -0500

    removed main method from Graph class

--- src/main/java/entities/Graph.java
+++ src/main/java/entities/Graph.java
@@ -90,46 +90,6 @@ public class Graph {
         }
         return output;
     }
-
-    public static void main(String[] args) {
-        Graph g = new Graph(7);
-        g.addEdge(0, 1);
-//        g.addEdge(0, 5);
-//        g.addEdge(0, 3);
-//
-//        g.addEdge(1, 0);
-//        g.addEdge(1, 2);
-//        g.addEdge(1, 4);
-//
-//        g.addEdge(2, 3);
-//        g.addEdge(2, 6);
-//
-//        g.addEdge(3, 1);
-//        g.addEdge(3, 4);
-//
-//        g.addEdge(5, 4);
-//
-//        g.addEdge(6, 4);
-
-
-        System.out.println(g);
-
-        int src = 1;
-        int dest = 0;
-
-        ArrayList<ArrayList<Integer>> paths =  g.allPaths(src,dest);
-        for (ArrayList<Integer> path : paths) {
-            String out = "";
-            for (Integer node : path) {
-                out += node;
-                if (node != dest) {
-                    out += " -> ";
-                }
-            }
-            System.out.println(out);
-        }
-    }
-
 }
 
 
