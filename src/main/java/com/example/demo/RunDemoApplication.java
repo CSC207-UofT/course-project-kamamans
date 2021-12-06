537cedfd39f95564b5676200903649381eba4738
sms-sudo <irock12022@gmail.com>
2021-12-06 13:40:38 -0500

    - added/updated javadoc and reformated code

--- src/main/java/com/example/demo/RunDemoApplication.java
+++ src/main/java/com/example/demo/RunDemoApplication.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+/**
+ *  Demo Class that launches the demo application
+ *  Consists of single main function to set up program
+ */
+
 public class RunDemoApplication {
     public static void main(String[] args){
         DemoApplication da = new DemoApplication();
